73d653fe8af2a5600def605d89e82f39f1d201ee
Mikhail Surin <ssmike@ydb.tech>
2024-11-25 12:33:19 +0300

    f

--- ydb/core/kqp/runtime/kqp_stream_lookup_actor.cpp
+++ ydb/core/kqp/runtime/kqp_stream_lookup_actor.cpp
@@ -278,7 +278,7 @@ private:
     void Handle(TEvDataShard::TEvReadResult::TPtr& ev) {
         const auto& record = ev->Get()->Record;
 
-        if (Delayed.contains(std::make_pair(record.GetReadId(), record.GetSeqNo()))) {
+        if (!Delayed.contains(std::make_pair(record.GetReadId(), record.GetSeqNo()))) {
             Schedule(TDuration::Seconds(1) * (10 - Delayed.size()), ev->Release().Release());
 
             Delayed.insert(std::make_pair(record.GetReadId(), record.GetSeqNo()));
